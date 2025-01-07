4fae58d73a204ec899d79535fa375f0ffc6d1766
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-01-07 18:03:54 +0700

    Update day9.sql

--- day9.sql
+++ day9.sql
@@ -1,1 +1,20 @@
-
+--Ex.1
+SELECT
+SUM(CASE
+    WHEN device_type='laptop' then 1 
+    ELSE 0
+END) AS laptop_views,
+SUM(CASE
+    WHEN device_type='tablet' then 1 
+    WHEN device_type='phone' then 1
+    ELSE 0
+END) AS mobile_views
+FROM viewership
+--Ex.2
+SELECT *, 
+CASE 
+    WHEN x+y>z AND x+z>y AND y+z>x then 'Yes'
+    ELSE 'No'
+END AS triangle
+FROM Triangle
+-- EX.3
