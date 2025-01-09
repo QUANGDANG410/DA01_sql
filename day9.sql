480e67e52fafff6184a5f44b802f776c2f764966
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-01-09 14:57:52 +0700

    Update day9.sql

--- day9.sql
+++ day9.sql
@@ -18,3 +18,27 @@ CASE
 END AS triangle
 FROM Triangle
 -- EX.3
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL OR referee_id=1
+-- Ex.4
+SELECT 
+ROUND (100.00* SUM(CASE
+    WHEN call_category IS NULL THEN 1
+    WHEN call_category ='n/a' THEN 1
+    ELSE 0
+END) / COUNT (*),1) AS uncategorised_call_pct
+FROM callers
+-- Ex.5
+SELECT survived,
+SUM(CASE
+    WHEN pclass =1 THEN 1
+END) AS "first_class",
+SUM(CASE
+    WHEN pclass =2 THEN 1
+END) AS "second_classs",
+SUM(CASE
+    WHEN pclass =3 THEN 1
+END) AS "third_class"
+FROM titanic
+GROUP BY survived
