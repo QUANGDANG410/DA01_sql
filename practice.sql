fed4fd815565eac7269599cc2ab62962fed5f81f
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-02-13 10:55:49 +0700

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -6,3 +6,11 @@ JOIN country AS b
 ON a.countrycode=b.code
 GROUP BY b.continent
 --Ex.2
+SELECT 
+ROUND(
+SUM (CASE WHEN t.signup_action='Confirmed' then 1 ELSE 0 END)*1.0/
+COUNT (t.signup_action),2) AS confirm_rate
+FROM texts AS t 
+LEFT JOIN emails AS e 
+ON t.email_id=e.email_id
+--Ex3:
