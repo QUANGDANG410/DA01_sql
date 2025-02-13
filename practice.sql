110ca46e54da8a9fa132da7290eaf2a0eb742978
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-02-13 18:11:57 +0700

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -14,3 +14,32 @@ FROM texts AS t
 LEFT JOIN emails AS e 
 ON t.email_id=e.email_id
 --Ex3:
+SELECT 
+  c.customer_id
+FROM products AS p LEFT JOIN customer_contracts AS c 
+ON p.product_id=c.product_id
+GROUP BY c.customer_id
+HAVING (COUNT(DISTINCT product_category))= (SELECT COUNT(DISTINCT (product_category)) FROM p)
+--Ex.4:
+SELECT
+    ab.age_bucket,
+  (  
+  ROUND(
+  SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END) /
+    SUM(CASE WHEN activity_type IN ('open', 'send') THEN time_spent ELSE 0 END)*100.0
+    ,2)
+  ) AS send_perc,
+    
+  ( 
+  ROUND(
+  SUM(CASE WHEN activity_type = 'open' THEN time_spent ELSE 0 END) /
+    SUM(CASE WHEN activity_type IN ('open', 'send') THEN time_spent ELSE 0 END)*100.0
+    ,2)
+  ) AS open_perc  
+    FROM
+      activities AS act
+    INNER JOIN
+      age_breakdown AS ab 
+    ON act.user_id = ab.user_id
+    GROUP BY
+      ab.age_bucket
