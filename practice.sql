14b665768412afbefea62c21a0a7e44b64b8ce0e
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-02-18 17:55:46 +0700

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -43,3 +43,12 @@ SELECT
     ON act.user_id = ab.user_id
     GROUP BY
       ab.age_bucket
+--Ex6: 
+SELECT p.product_name  ,
+    SUM(o.unit) AS unit
+FROM Products AS p
+JOIN Orders AS o
+ON p.product_id =o.product_id 
+WHERE order_date >= '2020-02-01'  AND order_date <= '2020-02-29'
+GROUP BY p.product_name 
+HAVING (SUM(o.unit)) >= 100
