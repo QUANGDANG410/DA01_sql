31ec876e75921ed7ca6f4629df9b752b9300d45a
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-02-25 18:11:29 +0700

    Update Practice 6.sql

--- Practice 6.sql
+++ Practice 6.sql
@@ -1,1 +1,68 @@
+Ex.1:
+WITH so_luong_job 
+AS
+(SELECT company_id, title, description,
+COUNT (job_id) AS count_job
+FROM job_listings
+GROUP BY company_id, title, description)
+
+SELECT COUNT(company_id) AS duplicate_companies
+FROM so_luong_job
+WHERE count_job>1
+Ex.2:
+WITH app AS
+(SELECT category, product, 
+SUM(spend) AS total_spend
+FROM product_spend
+WHERE category='appliance'
+GROUP BY category, product
+ORDER BY total_spend DESC
+LIMIT 2),
+elec AS
+(SELECT category, product, 
+SUM(spend) AS total_spend
+FROM product_spend
+WHERE category='electronics'
+GROUP BY category, product
+ORDER BY total_spend DESC
+LIMIT 2)
+SELECT * FROM app
+UNION ALL
+SELECT * FROM elec
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
