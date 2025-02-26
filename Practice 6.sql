9cd241348b72ddbfafa5796b5adfcb393da2aa58
QUANGDANG410 <dangquangnguyen693@gmail.com>
2025-02-26 11:38:14 +0700

    Update Practice 6.sql

--- Practice 6.sql
+++ Practice 6.sql
@@ -29,6 +29,29 @@ LIMIT 2)
 SELECT * FROM app
 UNION ALL
 SELECT * FROM elec
+Ex.3:
+WITH so_luong_cuoc_goi 
+AS 
+(SELECT policy_holder_id,
+COUNT (case_id) AS count_case
+FROM callers
+GROUP BY policy_holder_id)
+
+SELECT COUNT(policy_holder_id) AS policy_holder_count
+FROM so_luong_cuoc_goi
+WHERE count_case>=3
+Ex.4:
+WITH loc_data AS
+(SELECT p.page_id, p1.liked_date
+FROM pages AS p 
+LEFT JOIN page_likes AS p1
+ON p.page_id=p1.page_id)
+SELECT page_id
+FROM loc_data
+WHERE liked_date IS NULL 
+ORDER by page_id
+Ex.5:
+
 
 
 
